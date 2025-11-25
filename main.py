6befb2b003f285f5f6f30e7c8c40538a24086edf
Aleksandr Beliakov <genzksync0@gmail.com>
2025-11-25 14:24:36 +0300

    hide init db tables through engine

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ from core.models.base import Base
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    # async with engine.begin() as conn:
+    #     await conn.run_sync(Base.metadata.create_all)
 
     yield
 
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+    # async with engine.begin() as conn:
+    #     await conn.run_sync(Base.metadata.drop_all)
 
     await dispose()
 
